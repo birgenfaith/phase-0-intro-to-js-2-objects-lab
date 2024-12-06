15bb41cd9ed7ca21ab46e9ef4e14cf26968c26e8
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-12-06 17:35:56 +0300

    add solutions on index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Javi Lopez",
+    streetAddress: "123 Main St"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+return {
+    ...employee,
+    [key]:value
+};
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, updateEmployeeWithKeyAndValue) => {
+    employee[key] = updateEmployeeWithKeyAndValue;
+    return employee;
+};
+
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
